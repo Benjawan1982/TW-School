90385f50893d2e96beb06125dc99ed1930b5cf9d
masterUNG <phrombutr@gmail.com>
2016-07-24 10:35:01 +0700

    confirm dialog finish

--- app/src/main/java/tw/pitawanpor/twschool/SignUpActivity.java
+++ app/src/main/java/tw/pitawanpor/twschool/SignUpActivity.java
@@ -1,5 +1,7 @@
 package tw.pitawanpor.twschool;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +16,7 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText nameEditText,surnameEditText,roomEditText,userEditText,passwordEditText;
     private RadioGroup radioGroup;
     private RadioButton studentRadioButton, teacherRadioButton;
-    private String nameString, surnameString, roomString, userString, passwordString, statusString, QRcodeString;
+    private String nameString, surnameString, roomString, userString, passwordString, statusString = "1", QRcodeString;
 
 
     @Override
@@ -32,6 +34,27 @@ public class SignUpActivity extends AppCompatActivity {
         studentRadioButton = (RadioButton) findViewById(R.id.radioButton);
         teacherRadioButton = (RadioButton) findViewById(R.id.radioButton2);
 
+        //Radio Controller
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                switch (i) {
+                    case  R.id.radioButton:
+                        //for Student
+                        statusString = "1";
+
+                        break;
+                    case R.id.radioButton2:
+                        //for Teacher
+                        statusString = "0";
+
+                        break;
+
+                }   // switch
+
+            }   //onCheck
+        });
 
     }   //Main Method
 
@@ -53,9 +76,72 @@ public class SignUpActivity extends AppCompatActivity {
             Toast.makeText(this, "write all colump", Toast.LENGTH_SHORT).show();
         } else {
             //No Space
+            switch (Integer.parseInt(statusString)) {
 
+                case  0:
+                    QRcodeString = "teacher_" + userString;
+                    break;
+                case 1:
+                    QRcodeString = "student_" + userString;
+                    break;
+
+            }   //switch
+
+            confirmValue();
         }   //if
 
     }   //clickSignUp
+
+    private void confirmValue() {
+
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.icon_cow);
+        builder.setTitle("please check");
+        builder.setMessage("Name = " + nameString + "\n" +
+        "Surname = " + surnameString + "\n" +
+        "Status = " + shotStatus (statusString) +"\n" +
+        "Room =" + roomString + "\n" +
+        "User =" + userString + "\n" +
+        "Password =" + passwordString + "\n" +
+        "QRcode = " + QRcodeString);
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                uploadValueToServer();
+                dialogInterface.dismiss();
+            }
+
+
+        });
+        builder.show();
+
+    }   // confirmValue
+
+    private void uploadValueToServer() {
+
+    }   // upload
+
+    private String shotStatus(String statusString) {
+
+        String result = null;
+
+        switch (Integer.parseInt(statusString)) {
+            case  0:
+                result = "Teacher";
+                break;
+            case 1:
+                result = "Student";
+                break;
+        }
+        return result;
+    }
+
 
 }   //Main Class
