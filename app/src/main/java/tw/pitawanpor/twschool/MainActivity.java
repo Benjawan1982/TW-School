5baae35ae147a979d751542a48c72431001098ae
masterUNG <phrombutr@gmail.com>
2016-07-24 14:07:07 +0700

    Check Space

--- app/src/main/java/tw/pitawanpor/twschool/MainActivity.java
+++ app/src/main/java/tw/pitawanpor/twschool/MainActivity.java
@@ -5,14 +5,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        userEditText = (EditText) findViewById(R.id.editText6);
+        passwordEditText = (EditText) findViewById(R.id.editText7);
+
     }   // Main Method
+
+
+    public void clickSignIn (View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        // Check Space
+        if (userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            Toast.makeText(this, "Have Space ! Please All Every Blank",
+                    Toast.LENGTH_SHORT).show();
+
+        } else {
+            // No Space
+
+        } // if
+
+    }   // clickSignIn
 
     //Get Event from Click
     public void clickSignUpMain(View view) {
