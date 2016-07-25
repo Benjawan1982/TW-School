d9aceef41ac99e5ccafb5ed66f654e483695da83
masterUNG <phrombutr@gmail.com>
2016-07-25 15:31:45 +0700

    student success ?

--- app/src/main/java/tw/pitawanpor/twschool/StudentService.java
+++ app/src/main/java/tw/pitawanpor/twschool/StudentService.java
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -51,10 +52,19 @@ public class StudentService extends FragmentActivity implements OnMapReadyCallba
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //for Setup Center Map
+        final double twLat = 15.350713;   //Lattitude of tw
+        final double twLng = 100.491972;
+        LatLng latLng = new LatLng(twLat, twLng);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
+
+        //For Marker School
+        mMap.addMarker(new MarkerOptions()
+        .position(latLng)
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.tw_logo48))
+        .title("โรงเรียนตากฟ้าวิชาประสิทธิ์")
+        .snippet("Smart Clever"));
+
     }//onMap
 
 }   //Main Class
